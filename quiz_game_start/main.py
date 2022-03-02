72246ee7332f416f15e3b89c4538a6697c41083d
harish moturu <harish.moturu@uni-oldenburg.de>
2022-03-02 23:20:04 +0100

    Done with the project

--- quiz_game_start/main.py
+++ quiz_game_start/main.py
@@ -1,17 +1,18 @@
+from question_model import Question
 from data import question_data
-from question_model import QuestionBank
-from quiz_brain import QuizBrain as qb
+from quiz_brain import QuizBrain
 
-
-my_qb = QuestionBank(question_data)
 question_bank = []
-for x in question_data:
-    question = x["text"]
-    answer = x["answer"]
-    question_bank.append([question, answer])
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-input = True
-while input:
-    x = qb.next_question(question_bank)
+quiz = QuizBrain(question_bank)
 
+while quiz.still_has_questions():
+    quiz.next_question()
 
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- quiz_game_start/question_model.py
+++ quiz_game_start/question_model.py
@@ -1,12 +1,6 @@
 
+class Question:
 
-class QuestionBank:
-    def __init__(self, question_text, question_answer):
-        self.question = question_text
-        self.answer = question_answer
-
-
-    def question_bank(self, question):
-        x = question.keys()
-        y = question.values()
-        return x,y
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
