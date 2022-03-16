827ef847dcbb02f94edec7b8a7b8fda103deb51c
harish moturu <harish.moturu@uni-oldenburg.de>
2022-03-17 00:43:03 +0100

    WIP: snake game starting instances are created

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,21 +1,31 @@
 from turtle import Turtle, Screen
+import time
 
 
 screen = Screen()
 screen.setup(width=600, height= 600)
 screen.bgcolor('black')
 
-segment1 = Turtle('square')
-segment1.color('white')
-segment1.goto((0,0))
+positions = [(0,0), (-20,0),(-40,0) ]
+segments = []
+for position in positions:
+    new_segment = Turtle('square')
+    new_segment.color('white')
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
 
-segment2 = Turtle('square')
-segment2.color('white')
-segment2.goto((-20,0))
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-segment3 = Turtle('square')
-segment3.color('white')
-segment3.goto((-40,0))
+    for seg_num in range(len(segments) -1 , 0, -1):
+        new_x = segments[seg_num -1].xcor()
+        new_y = segments[seg_num -1].ycor()
+        segments[seg_num].goto(new_x,new_y)
+    segments[0].forward(20)
+
 
 
 screen.exitonclick()
