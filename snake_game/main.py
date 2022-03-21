96b78f08935689d54a5bf189de7f483a89c88900
harish moturu <harish.moturu@uni-oldenburg.de>
2022-03-21 10:08:35 +0100

    WIP: one bug related to the continues snake movement should be removed

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,7 +1,8 @@
 import time
 
-from turtle import Turtle, Screen
+from turtle import  Screen
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height= 600)
@@ -10,9 +11,13 @@ screen.title("My snake game")
 screen.tracer(0)
 
 snake = Snake()
-screen.onkey(snake.up())
-snake.down()
-snake.right()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
@@ -20,8 +25,9 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-
-
+    #detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
 
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,10 +1,14 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
 
 # python class in pascal case
 STARTING_POSITIONS = [(0,0), (-20,0),(-40,0)]
 MOVING_DISTANCE = 20
-class Snake:
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
+class Snake:
 
     def __init__(self):
         #attributes should be defined here
@@ -14,11 +18,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle('square')
-            new_segment.color('white')
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) -1 , 0, -1):
@@ -28,14 +38,18 @@ class Snake:
             self.segments[0].forward(MOVING_DISTANCE)
 
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.head.setheading(180)
+        if self.head.heading != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.head.setheading(270)
+        if self.head.heading != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.head.setheading(360)
+        if self.head.heading != LEFT:
+            self.head.setheading(RIGHT)
 
