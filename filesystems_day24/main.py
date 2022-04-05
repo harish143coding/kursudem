ffd0b87ca2664ce192d8085cf9310b22ca4ce973
harish moturu <harish.moturu@uni-oldenburg.de>
2022-04-05 11:38:08 +0200

    WIP: file system module in progress

--- filesystems_day24/main.py
+++ filesystems_day24/main.py
@@ -1,5 +1,12 @@
-file = open("my_file.txt")
-contents = file.read()
-x = 6
-print(contents)
-print(type(x))
+
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     x = 6
+#     print(contents)
+#     print(type(x))
+
+with open("my_file.txt", mode = 'w') as file:
+    # modes 'w' write, 'r' read, 'a' append
+    file.write(input("what is the input"))
+
